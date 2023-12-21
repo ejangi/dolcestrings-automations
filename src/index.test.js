2493e354f6e7db5cabf14c054a8827c454f456fd
James Angus <james@ejangi.com>
2023-12-22 07:23:28 +1000

    Removed the success webhook from the index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -18,8 +18,7 @@ test('Function can be invoked', async () => {
         //make sure to serialize your JSON body
         body: JSON.stringify({
             current_products_filename: 'current_products.json',
-            new_prices_filename: 'new_prices.json',
-            //success_webhook: 'https://hook.eu1.make.com/i2ca8f2h6rxfabfk01yagdm1hw8jw673'
+            new_prices_filename: 'new_prices.json'
         })
     })
     .then( async (response) => {
